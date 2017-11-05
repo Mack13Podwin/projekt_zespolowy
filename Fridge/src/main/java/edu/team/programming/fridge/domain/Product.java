943112f50c9989599cb8b7db402cc1e996b6262b
Lukasz Mazurkiewicz <gepird@gmail.com>
2017-11-05 14:44:53 +0100

    dołożony pierwszy handler zapytań zmieniono properties-y tak, żeby można było odpalać na własnych ustawieniach

---
+++ Fridge/src/main/java/edu/team/programming/fridge/domain/Product.java
@@ -0,0 +1,11 @@
+package edu.team.programming.fridge.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+    private String barCode;
+    private String name;
+}
---
+++ Fridge/src/main/java/edu/team/programming/fridge/rest/camera/CameraController.java
@@ -0,0 +1,18 @@
+package edu.team.programming.fridge.rest.camera;
+
+import edu.team.programming.fridge.domain.Product;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/camera")
+public class CameraController {
+
+    @RequestMapping(value="/product", method= RequestMethod.PUT)
+    public Product putProduct(@RequestBody String barCode){
+        return new Product(barCode, "mleko");
+    }
+
+}

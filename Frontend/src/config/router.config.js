aea93c2f535047f8877de2fecf9aef379c24e488
Lukasz Mazurkiewicz <gepird@gmail.com>
2017-11-27 17:44:44 +0100

    dołożenie frontendu

---
+++ Frontend/src/config/router.config.js
@@ -0,0 +1,27 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('app')
+        .config(config);
+
+    function config($routeProvider, $locationProvider){
+        $locationProvider.hashPrefix("");
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'src/home/home.html',
+            })
+            .when('/fridgeItems', {
+                templateUrl: 'src/fridgeItems/fridgeItems.html',
+            })
+            .when('/shoppingList', {
+                templateUrl: 'src/shoppingList/shoppingList.html',
+            })
+            .when('/suggestedRecipe', {
+                templateUrl: 'src/suggestedRecipe/suggestedRecipe.html',
+            })
+            .otherwise({
+                redirectTo: '/home'
+            });
+    }
+})();

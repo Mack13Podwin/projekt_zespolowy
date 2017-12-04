c3675b6f68d6fece0db5e964efa0509ac8519aab
sokuzs1 <sokuzs1@gmail.com>
2017-12-04 18:44:03 +0100

    Zmiana tesseracta

--- Fridge/cvtest/src/main/java/tools/TessUtils.java
+++ Fridge/cvtest/src/main/java/tools/TessUtils.java
@@ -1,8 +1,6 @@
 package tools;
 
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
+
 
 import java.awt.image.BufferedImage;
 import java.nio.file.Paths;
@@ -10,19 +8,49 @@ import java.nio.file.Paths;
 public class TessUtils {
     private ITesseract instance;
     public TessUtils(){
-        instance = new Tesseract();
-        instance.setDatapath(Paths.get("tessdata").toAbsolutePath().toString());
-        instance.setLanguage("bpd+dotc+dots+hydro+jd+eng");
-        instance.setTessVariable("tessedit_char_whitelist", "0123456789-/.");
+        ITessAPI.TessBaseAPI handle = TessAPI1.TessBaseAPICreate();
+        String datapath = Paths.get("tessdata").toAbsolutePath().toString();
+        String language = "bpd+dotc+dots+hydro+jd+eng";
+        TessAPI1.TessBaseAPIInit3(handle, datapath, language);
+        TessAPI1.TessBaseAPISetVariable(handle, "tessedit_char_whitelist", "0123456789-/.");
     }
-    String getText(BufferedImage image){
-        try{
-            String result = instance.doOCR(image);
-            return result;
+    String[][] getAllChoices(BufferedImage image){
+        ByteBuffer buf = ImageIOHelper.convertImageData(image);
+        int bpp = image.getColorModel().getPixelSize();
+        int bytespp = bpp / 8;
+        int bytespl = (int) Math.ceil(image.getWidth() * bpp / 8.0);
+        TessAPI1.TessBaseAPISetPageSegMode(handle, ITessAPI.TessPageSegMode.PSM_SINGLE_BLOCK);
+        TessAPI1.TessBaseAPISetImage(handle, buf, image.getWidth(), image.getHeight(), bytespp, bytespl);
+        TessAPI1.TessBaseAPISetVariable(handle, "save_blob_choices", "T");
+        TessAPI1.TessBaseAPIRecognize(handle, null);
+        ITessAPI.TessResultIterator ri = TessAPI1.TessBaseAPIGetIterator(handle);
+        int level = ITessAPI.TessPageIteratorLevel.RIL_SYMBOL;
 
-        } catch (TesseractException e) {
-            System.err.println(e.getMessage());
+        int i = 0;
+        int j = 0;
+        int jmax = 0;
+
+        String[][] symbols = new String[100][10];
+        double[][] confidences = new double[100][10];
+        if (ri != null) {
+            do {
+                Pointer symbol = TessAPI1.TessResultIteratorGetUTF8Text(ri, level);
+                if (symbol != null) {
+                    ITessAPI.TessChoiceIterator ci = TessAPI1.TessResultIteratorGetChoiceIterator(ri);
+                    do {
+                        String choice = TessAPI1.TessChoiceIteratorGetUTF8Text(ci);
+                        symbols[i][j] = choice;
+                        confidences[i][j] = TessAPI1.TessChoiceIteratorConfidence(ci);
+                        j++;
+                        if(jmax<j)
+                            jmax=j;
+                    } while (TessAPI1.TessChoiceIteratorNext(ci) == ITessAPI.TRUE);
+                    TessAPI1.TessChoiceIteratorDelete(ci);
+                }
+                i++;
+                j=0;
+                TessAPI1.TessDeleteText(symbol);
+            } while (TessAPI1.TessResultIteratorNext(ri, level) == ITessAPI.TRUE);
+            return symbols;
         }
-        return null;
     }
-}

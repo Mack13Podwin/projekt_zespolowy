11c3b80547906b3a5ee1cf109ef8f0b0ed671566
Mack13Podwin <maciej.podwinski@gmail.com>
2018-01-08 19:54:33 +0100

    Adding: Adding Bartek's API request

--- Backend/src/main/java/edu/team/programming/fridge/rest/ui/UIController.java
+++ Backend/src/main/java/edu/team/programming/fridge/rest/ui/UIController.java
@@ -13,9 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping(value="/ui")
@@ -68,5 +74,39 @@ public class UIController {
     @RequestMapping(value = "/expired/{fridgeId}", method=RequestMethod.POST)
     public List<Product> getExpired(@PathVariable String fridgeId){
         return productRepository.findByFridgeidAndExpirationdateBeforeAndRemovingdateNotNull(fridgeId,new Date());
+    }
+
+    @RequestMapping(value = "/recipe/{fridgeId}", method=RequestMethod.GET)
+    public List<String> getRecipe(@PathVariable String fridgeId)
+    {
+        List<Product> products = productRepository.findByFridgeid(fridgeId);
+        ArrayList<String> types = new ArrayList<>();
+        ArrayList<String> outputLines = new ArrayList<>();
+        for(Product product : products)
+            types.add(product.getType());
+        Random rand = new Random();
+        String ing = types.get(rand.nextInt(types.size()));
+        try {
+            URL url = new URL("https://api.edamam.com/search?q=" + ing + "&app_id=996b3dbc&app_key=c510d40f64b5a45bd6b2baaa13aa5a2f&from=0&to=1");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+            String output;
+            while ((output = br.readLine()) != null) {
+                outputLines.add(output);
+            }
+            conn.disconnect();
+            return outputLines;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        outputLines.add("Error");
+        return outputLines;
     }
 }
